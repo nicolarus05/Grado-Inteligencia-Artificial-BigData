b00ec02624e85ea59a58f5db8800d11349a1c380
nicolarus05 <ngh2605@gmail.com>
2025-09-25 18:00:09 +0200

    Introduccion a la IA

---
+++ Modelos-de-IA/Apuntes.py
@@ -0,0 +1,27 @@
+#Tema 1 Introduzcion a la Inteleigencia Artificial
+'''
+La inteligencia artificial es un campo de la informatica que busca crear sistemas capaces de realizar tareas que normalmente
+requieren inteligencia humana, como el aprendizaje, el razonamiento, la percepcion y la toma de decisiones.
+
+Hay 2 tipos de IA:
+- IA debil: Sistemas diseñados para realizar tareas específicas, como asistentes virtuales o sistemas de recomendación.
+- IA fuerte: Sistemas con capacidades cognitivas generales similares a las humanas, capaces de entender, 
+             aprender y aplicar conocimientos en una amplia variedad de contextos.
+
+Un modelo de IA es una representación matemática o algorítmica de un sistema de IA que se utiliza para realizar tareas específicas.
+
+Tipos de datos: 
+- Estructurados: Datos organizados en un formato predefinido, como tablas o bases de datos.
+- No estructurados: Datos que no tienen un formato predefinido, como texto, imágenes o videos.
+
+Un algoritmo es un conjunto de instrucciones paso a paso que se utilizan para resolver un problema o realizar una tarea específica.
+Tipos de algoritmos:
+- Clasificacion: Asignar una etiqueta o categoria a un conjunto de datos.
+- Regresion: Predecir un valor continuo basado en datos de entrada.
+- Clustering: Agrupar datos similares en conjuntos o clusters.
+
+Proceso de entrenamiento de un modelo de IA:
+- Entrenamiento: El modelo aprende a partir de un conjunto de datos de entrenamiento.
+- Evaluacion: Evaluar el rendimiento del modelo utilizando un conjunto de datos de validacion.
+- Ajuste: Ajustar los parametros del modelo para mejorar su rendimiento.
+'''
